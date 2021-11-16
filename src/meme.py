06abaf0ba770bc71fd6eea35f53d8a8969e4ddb5
shaguargeffon <shaguarhan123@gmail.com>
2021-11-17 00:05:41 +0100

    Development-meme

--- src/meme.py
+++ src/meme.py
@@ -1,29 +1,33 @@
+"""A python module to generate a random meme using CLI."""
+
 import os
 import random
-
-# @TODO Import your Ingestor and MemeEngine classes
+import argparse
+from QuoteEngine.Ingestor import Ingestor
+from QuoteEngine.QuoteModel import QuoteModel
+from MemeGenerator.MemeGenerator import MemeEngine
 
 
 def generate_meme(path=None, body=None, author=None):
-    """ Generate a meme given an path and a quote """
+    """Generate a meme given an path and a quote."""
     img = None
     quote = None
 
     if path is None:
-        images = "./_data/photos/dog/"
+        images = "./_data/photos/Naruto/"
         imgs = []
         for root, dirs, files in os.walk(images):
             imgs = [os.path.join(root, name) for name in files]
 
         img = random.choice(imgs)
     else:
-        img = path[0]
+        img = path
 
     if body is None:
-        quote_files = ['./_data/DogQuotes/DogQuotesTXT.txt',
-                       './_data/DogQuotes/DogQuotesDOCX.docx',
-                       './_data/DogQuotes/DogQuotesPDF.pdf',
-                       './_data/DogQuotes/DogQuotesCSV.csv']
+        quote_files = ['./_data/NarutoQuotes/NarutoQuotesTXT.txt',
+                       './_data/NarutoQuotes/NarutoQuotesDOCX.docx',
+                       './_data/NarutoQuotes/NarutoQuotesPDF.pdf',
+                       './_data/NarutoQuotes/NarutoQuotesCSV.csv']
         quotes = []
         for f in quote_files:
             quotes.extend(Ingestor.parse(f))
@@ -39,10 +43,22 @@ def generate_meme(path=None, body=None, author=None):
     return path
 
 
+def write_share_meme_email(meme_path):
+    """Write meme path to txt file."""
+    with open('share.txt', 'w') as f:
+        f.write(meme_path)
+
+
 if __name__ == "__main__":
-    # @TODO Use ArgumentParser to parse the following CLI arguments
-    # path - path to an image file
-    # body - quote body to add to the image
-    # author - quote author to add to the image
-    args = None
-    print(generate_meme(args.path, args.body, args.author))
+    parser = argparse.ArgumentParser(description="Generate a meme.")
+    parser.add_argument('--path', type=str, default=None, help="Path to image")
+    parser.add_argument('--body', type=str,
+                        default=None,
+                        help="body to be added to image")
+    parser.add_argument('--author', type=str,
+                        default=None,
+                        help="author to be added to image")
+    args = parser.parse_args()
+    meme_output = generate_meme(args.path, args.body, args.author)
+    print(meme_output)
+    write_share_meme_email(meme_output)
